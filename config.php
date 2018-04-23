21a6d7e2252dd14ba442f44830abfeca545a5ff4
kieracoyne <38110612+kieracoyne@users.noreply.github.com>
2018-04-22 22:14:04 -0400

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  define('DB_HOST', "localhost:8889"); //for windows pc, change it to "localhost"
-  define('DB_USER', "alan");  //you have to change the user name and password to yours to make it work
-  define('DB_PASS', "bit4444");
-  define('DB_NAME', "hokie");
-?>
