6ea1612985a8a9cfea6986da7ff43952f7d389f3
kieracoyne <38110612+kieracoyne@users.noreply.github.com>
2018-04-25 10:08:01 -0400

    Create HMdisplaytables.php

---
+++ HMdisplaytables.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+  <style type="text/css">
+  .colors  {background-color:#fff0b3;color:black;vertical-align:top}
+  
+  </style>
+  <title> Display: When you sleep!</title>
+</head>
+<body>
+  <?php
+  $id = 0;
+  if(isset($_GET['id'])) $id=$_GET['id'];
+
+  require_once("db.php");
+
+  $sql = "";
+  if($id==0)
+    $sql ="select * from students";
+  else {
+    $sql ="select * from students where clean=".$id;
+    }
+  $result = $mydb->query($sql);
+
+
+
+
+  echo "<table> ";
+  echo "<thead class =  'colors'>";
+  echo "<th>First Name</th><th>Last Name</th><th>Clean</th>";
+  echo "</thead>";
+
+    while($row = mysqli_fetch_array($result)) {
+      echo "<tr > </tr>";
+      echo "<td>".$row["First_Name"]."</td><td>".$row["Last_Name"]."</td><td>".$row["Clean"]."</td>";
+
+      echo "</tr>";
+    }
+
+  ?>
+</body>
+</html>
