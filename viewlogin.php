0a94d55c0ed22328f4040aae9ac05d29b14ba7be
alexc97 <38110600+alexc97@users.noreply.github.com>
2018-04-25 21:25:55 -0400

    Create viewlogin.php

---
+++ viewlogin.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>HokieMatch: Home </title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+      <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <script src="jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">    </script>
+
+    <title>All Login Information</title>
+    <style>
+      table, td {
+        border: 1px solid orange;
+      }
+      table {
+        border-collapse: collapse;
+        empty-cells: show;
+        display:
+      }
+      th, td:first-child{
+        color: orange;
+        background-color: maroon;
+      }
+      td {
+        width: 15em;
+        height: 20px;
+        color: black;
+        background-color: rgb(255, 204, 102);
+      }
+    </style>
+  </head>
+  <p>
+    <body>
+
+
+
+        <a href="HMHome.html"><img src="Hokiematchlogo.png"  HEIGHT="75" WIDTH="250" /></a>
+
+        <a  href="HMStudentlogin.html" align="right">Student Log In</a>
+
+
+
+
+    <p>
+
+    <nav>
+      <ul class="nav nav-pills">
+        <li role="presentation" class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+          Home <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li role="presentation"><a href="HMHome.html">Home</a></li>
+          <li role="presentation"><a href="HMHousingoptions.html">Housing Options</a></li>
+          <li role="presentation"><a href="HMExampleprofile.html">Example Profile Match-Up</a></li>
+        </ul>
+      </li>
+
+      <li role="presentation" class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        Personal Information <span class="caret"></span>
+      </a>
+      <ul class="dropdown-menu">
+        <li role="presentation"><a href="personalinfo.html">Personal Information</a></li>
+        <li role="presentation"><a href="roommatePref.html">Roommate Preferences</a></li>
+        <li role="presentation"><a href="apartmentPref.html">Apartment Preferences</a></li>
+      </ul>
+    </li>
+
+    <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Admin Login <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      <li role="presentation"><a href="#">Admin Login</a></li>
+      <li role="presentation"><a href="#">Profile Management</a></li>
+      <li role="presentation"><a href="#">Student Profiles</a></li>
+      <li role="presentation"><a href="updateApartment.html">Apartment Editing</a></li>
+      <li role="presentation"><a href="#">Apartment Renewal</a></li>
+    </ul>
+    </li>
+
+      </ul>
+    </nav>
+  <br />
+  <br />
+  </p>
+
+<p>
+
+
+</p>
+
+  <body>
+    <?php
+    require_once("db.php");
+    $sql = "select * from login";
+    $result = $mydb->query($sql);
+    echo "<table>";
+    echo "<thead>";
+    echo "<th>Login ID</th><th>Name</th><th>Username</th><th>Password</th>";
+    echo "</thead>";
+      while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td> ".$row["login_ID"]."</td><td>".$row["name"]."</td><td>".$row["username"]."</td><td>".$row["password"]."</td>";
+        echo "</tr>";
+      }
+      echo "</table>"
+    ?>
+  </body>
+</html>
